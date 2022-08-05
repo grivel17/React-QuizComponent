f9dcfea38f3a9c27fd8b5f58a5d647a5d7443270
Tomasz Wojciechowski <tomasz.wojciechowski@sollers.eu>
2022-08-05 19:02:49 +0200

    module 1

---
+++ src/Quiz.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+let quizData = require("./quiz_data.json");
+export default class Quiz extends Component {
+  render() {
+    return (
+      <div className="QuizQuestion">
+        {quizData.quiz_questions[0].instruction_text}
+      </div>
+    );
+  }
+}
+
+// import React, { Component } from "react";
+
+// let quizData = require("./quiz_data.json");
+
+// export default class Quiz extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       quiz_position: 1,
+//     };
+// }
+//   render()
+//     return {quizData.quiz_questions[0].instruction_text}</div>;
+
+// }
