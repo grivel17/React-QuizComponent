f045379c72ca07e57fcf443891017deb65eb12e5
Tomasz Wojciechowski <tomasz.wojciechowski@sollers.eu>
2022-08-06 19:06:14 +0200

    module 6 v2

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -13,6 +13,9 @@ class QuizQuestion extends Component {
     handleClick(buttonText){
         if(buttonText === this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler()
+            this.setState(()=> {return{incorrectAnswer: false}})
+        } else {
+            this.setState(()=> {return{incorrectAnswer: true}})
         }
     }
 
