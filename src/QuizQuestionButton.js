b77d6981c60170ea2d1c9555e988b8e82e5f7a02
Tomasz Wojciechowski <tomasz.wojciechowski@sollers.eu>
2022-08-06 18:36:06 +0200

    module 5 v4

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 
 class QuizQuestionButton extends Component {
 
-  handelClick() {
+  handleClick() {
 
   }
   render() {
     return (
       <li>
-        <button onClick={this.handelClick.bind(this)}>{this.props.button_text}</button>
+        <button onClick={this.handleClick.bind(this)}>{this.props.button_text}</button>
       </li>
     );
   }
